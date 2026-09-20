22eb3b79aa7193135ac7daf099f4a3ea285d5002
Abdelhameed Al Asadee <abdelhameed81@hotmail.co.uk>
2026-09-20 21:41:19 +0100

    Added two test for valid and invalid status transition

--- test/test_application.py
+++ test/test_application.py
@@ -1,7 +1,17 @@
+import pytest
 from app.application_status import ApplicationStatus
+from app.exceptions import InvalidStatusTransitionError
 
 
 def test_application(application, candidate, job) -> None:
     assert application.candidate == candidate
     assert application.job == job
     assert application.get_status() == ApplicationStatus.APPLIED
+
+def test_application_can_change_to_valid_status(application) -> None:
+    application.change_status(ApplicationStatus.UNDER_REVIEW)
+    assert application.get_status() == ApplicationStatus.UNDER_REVIEW
+
+def test_application_rejects_invalid_status_transition(application) -> None:
+    with pytest.raises(InvalidStatusTransitionError):
+        application.change_status(ApplicationStatus.INTERVIEW)
