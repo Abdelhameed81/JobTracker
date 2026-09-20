4148e01e5ac397dbc5d8edd72fe4b3962b0989dc
Abdelhameed Al Asadee <abdelhameed81@hotmail.co.uk>
2026-09-20 17:59:24 +0100

    Made the phone argument as str

--- app/candidate.py
+++ app/candidate.py
@@ -1,8 +1,8 @@
 class Candidate:
     """ This class contains the candidate details """
 
-    def __init__(self, name: str, email: str, phone: int, cv: str) -> None:
-        self.name =  name
+    def __init__(self, name: str, email: str, phone: str, cv: str) -> None:
+        self.name = name
         self.email = email
         self.phone = phone
         self.cv = cv
