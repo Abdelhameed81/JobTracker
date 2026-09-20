f50fdd30d609bd574fe10fdc14f50c0104173d06
Abdelhameed Al Asadee <abdelhameed81@hotmail.co.uk>
2026-09-20 18:23:55 +0100

    Made the job class as dataclass

--- app/job.py
+++ app/job.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
 class Job:
     """ This class contains the company details """
-    def __init__(self, title: str, company_name: str, location: str, work_arrangement: str, salary: str, company_description: str, responsibilities: str) -> None:
-        self.title = title
-        self.company_name = company_name
-        self.location = location
-        self.work_arrangement = work_arrangement
-        self.salary = salary
-        self.company_description = company_description
-        self.responsibilities = responsibilities
+    title: str
+    company_name: str
+    location: str
+    work_arrangement: str
+    salary: str
+    company_description: str
+    responsibilities: str
