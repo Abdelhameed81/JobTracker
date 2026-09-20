24c84404c68833bb1127b1c438afb07b5949d2c2
Abdelhameed Al Asadee <abdelhameed81@hotmail.co.uk>
2026-09-20 18:19:48 +0100

    Made the candidate class as dataclass

--- app/candidate.py
+++ app/candidate.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
 class Candidate:
     """ This class contains the candidate details """
-
-    def __init__(self, name: str, email: str, phone: str, cv: str) -> None:
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.cv = cv
+    name: str
+    email: str
+    phone: str
+    cv: str
