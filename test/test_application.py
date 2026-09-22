15d906fa663bdb1dbbd4276ff3b906077feac172
Abdelhameed Al Asadee <abdelhameed81@hotmail.co.uk>
2026-09-22 16:05:24 +0100

    Test formatting

--- test/test_application.py
+++ test/test_application.py
@@ -9,24 +9,36 @@ def test_application(application, candidate, job) -> None:
     assert application.job == job
     assert application.get_status() == ApplicationStatus.APPLIED
 
+
+
 def test_application_can_change_to_valid_status(application) -> None:
     application.change_status(ApplicationStatus.UNDER_REVIEW)
     assert application.get_status() == ApplicationStatus.UNDER_REVIEW
 
+
+
 def test_application_rejects_invalid_status_transition(application) -> None:
     with pytest.raises(InvalidStatusTransitionError):
         application.change_status(ApplicationStatus.INTERVIEW)
+
+
 
 @pytest.mark.parametrize("application_status", list(ApplicationStatus))
 def test_closed_application_does_not_accept_further_status_change(closed_application, application_status) -> None:
     with pytest.raises(InvalidStatusTransitionError):
         closed_application.change_status(application_status)
 
+
+
 def test_applied_at_is_datetime(application) -> None:
     assert isinstance(application.applied_at, datetime)
 
+
+
 def test_applied_at_is_actually_utc(application) -> None:
     assert application.applied_at.tzinfo == timezone.utc
+
+
 
 def test_applied_at_is_read_only(application) -> None:
     with pytest.raises(AttributeError):
